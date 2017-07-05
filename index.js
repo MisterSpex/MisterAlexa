b007ea0c0a19914aef3c896b8a7449fd4bfbb605
André Neubauer <andre.neubauer@googlemail.com>
2017-07-05 16:31:54 +0200

    Change handler to give a default answer

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-'use strict';
 var Alexa = require('alexa-sdk');
-var appId = '';
+const APP_ID = "amzn1.ask.skill.ac4dda67-94cd-4676-a49f-290e22135397"
 
 exports.handler = function(event, context, callback){
-  var alexa = Alexa.handler(event, context, callback);
-  alexa.registerHandlers(handler);
-   alexa.appId = appId;
-  alexa.execute();
+    var alexa = Alexa.handler(event, context, callback);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
 
-var handler = {
-  'FindNextPOorStoreIntent': function() {
-    var defaultResponse = 'The next Mister Spex Store is at the Alexa shopping center'
-    this.emit(':tell', defaultResponse);
+var handlers = {
+  'MisterSpex': function () {
+        this.emit(':tell', 'Mister Spex hat Geschäfte in Berlin, Bremen und Oberhausen.');
+    },
 
-  }
+    'Unhandled': function() {
+      this.emit(':ask', 'Ich kann dich leider nicht verstehen');
+    },
 
 }
+
+
+// https://api.amazonalexa.com/v1/devices/{deviceId}/settings/address/countryAndPostalCode
